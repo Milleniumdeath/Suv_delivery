b04c0e16114f234b6713c4b8ac4f480c1f03d5a4
Millenium death <milleniumdeath90@gmail.com>
2025-08-28 16:07:32 +0500

    Yangilandi.

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('buyurtma/', BuyurtmaListCreateAPIView.as_view(),),
     path('haydovchilar/', HaydovchiListAPIView.as_view(), name='haydovchi-list'),
-    path('haydovchi/', HaydovchiDetailsSerializer.as_view(), name='haydovchi-retrieve'),
+    path('haydovchi/', HaydovchiRetrieveAPIView.as_view(), name='haydovchi-retrieve'),
     path('suvlar/', SuvAPIView.as_view(), name='suv'),
 
 
